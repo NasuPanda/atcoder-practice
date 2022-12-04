fa5adef977b510398edc5592c1f7e487020ced74
Westen0511 <xiyuanjunzhi@gmail.com>
2022-12-04 14:29:11 +0900

    Update compile.sh

--- cpp/snipets/compile.sh
+++ cpp/snipets/compile.sh
@@ -1,4 +1,8 @@
 # 入力された cpp のファイルをコンパイルし、実行可能ファイルを /bin 配下に出力する
+OUTDIR="bin"
+mkdir -p $OUTDIR
 #$1: cpp のファイル名
 FILENAME=`basename $1 .cpp`
-g++ $1 -o bin/$FILENAME
+# -o: 出力ファイル名
+# -std: 使用するC++のバージョン
+g++ $1 -o $OUTDIR/$FILENAME -std=c++17

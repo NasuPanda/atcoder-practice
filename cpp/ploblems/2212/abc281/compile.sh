7144822471795c117b572399f9a23d0f7465edb8
Westen0511 <xiyuanjunzhi@gmail.com>
2022-12-11 19:50:32 +0900

    Update compile.sh to change creating executable file name

--- cpp/ploblems/2212/abc281/compile.sh
+++
@@ -1,8 +0,0 @@
-# 入力された cpp のファイルをコンパイルし、実行可能ファイルを /bin 配下に出力する
-OUTDIR="bin"
-mkdir -p $OUTDIR
-#$1: cpp のファイル名
-FILENAME=`basename $1 .cpp`
-# -o: 出力ファイル名
-# -std: 使用するC++のバージョン
-g++ $1 -o $OUTDIR/$FILENAME -std=c++17
--- cpp/snipets/compile.sh
+++ cpp/snipets/compile.sh
@@ -1,8 +1,7 @@
 # 入力された cpp のファイルをコンパイルし、実行可能ファイルを /bin 配下に出力する
-OUTDIR="bin"
 mkdir -p $OUTDIR
 #$1: cpp のファイル名
 FILENAME=`basename $1 .cpp`
 # -o: 出力ファイル名
 # -std: 使用するC++のバージョン
-g++ $1 -o $OUTDIR/$FILENAME -std=c++17
+g++ $1 -o a.out -std=c++17

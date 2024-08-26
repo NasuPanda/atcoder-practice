d170edb3b9bd50db0610af92f35182614e95ee5e
NasuPanda <xiyuanjunzhi@gmail.com>
2024-08-26 18:05:35 +0900

    Boot camp for beginners: modify templete for medium problems

--- cpp/ploblems/2408/practice/bootcamp-for-beginners/medium_049.cpp
+++ cpp/ploblems/2408/practice/bootcamp-for-beginners/medium_049.cpp
@@ -1,12 +1,10 @@
 #include <bits/stdc++.h>
 using namespace std;
-/* alias */
-using ull = unsigned long long;
-using ll = long long;
-using vi = vector<int>;
-using vl = vector<long>;
-using vll = vector<long long>;
-#define rep(i, n) for (int i = 0; i < (int)(n); i++)
+#define rep(i,a,b) for(int i=a;i<b;i++)
+#define all(x) (x).begin(),(x).end()
+typedef long long ll; const int inf = INT_MAX / 2; const ll infl = 1LL << 60;
+template<class T>bool chmax(T& a, const T& b) { if (a < b) { a = b; return 1; } return 0; }
+template<class T>bool chmin(T& a, const T& b) { if (b < a) { a = b; return 1; } return 0; }
 
 int main() {
 
